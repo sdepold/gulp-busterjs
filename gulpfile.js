592417d82c9ba0e9afca8a8d3917da7c409f2d17
Sascha Depold <sascha@depold.com>
2014-10-20 08:57:01 +0200

    Fix async tests

--- gulpfile.js
+++ gulpfile.js
@@ -23,13 +23,16 @@ gulp.task("lint", function () {
 gulp.task("test", ["test-unit", "test-integration"], noop);
 
 gulp.task("test-unit", function () {
-  gulp
+  return gulp
     .src("./test/unit/**/*-test.js")
-    .pipe(buster({name: "unit"}));
+    .pipe(buster({ name: "unit" }));
 });
 
-gulp.task("test-integration", function () {
-  gulp
+//
+// Add test-unit as dependency ensures the serial order of the tests.
+//
+gulp.task("test-integration", ["test-unit"], function () {
+  return gulp
     .src("./test/integration/**/*-test.js")
-    .pipe(buster({name: "integration"}));
+    .pipe(buster({ name: "integration" }));
 });
