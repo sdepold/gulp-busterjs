d774e90c506ffa7cbba4e22e4b6d7b56473fb4b0
Johannes Würbach <johannes.wuerbach@googlemail.com>
2015-11-26 18:34:04 +0100

    Look at the actual return code instead of parsing stdout

--- test/tmp/failing-test.js
+++ test/tmp/failing-test.js
@@ -7,7 +7,6 @@ var it       = buster.spec.it;
 
 describe('gulp-busterjs', function () {
   it('throws when I print a runtime error', function () {
-    console.log('1 test, 0 assertions, 1 runtime ... 1 failure');
-    expect(true).toEqual(true);
+    expect(false).toEqual(true);
   });
 });
